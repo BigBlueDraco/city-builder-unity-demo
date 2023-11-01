145e40fb0bed91a24fcdb0ba63e55ae0a6220185
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-11-01 17:38:30 +0200

    feat abstract class Conectors

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Random = System.Random;
 using Debug = UnityEngine.Debug;
 
+
 public class Cordinats
 {
 	public int x;
@@ -172,4 +173,32 @@ public class Grid
 		}
 		return _cells;
 	}
+}
+
+interface IRotatebel
+{
+	int RotationPosition{ get; }
+	public abstract void RotateLeft(int times);
+	public abstract void RotateRight(int times);
+}
+abstract class Conectors: IRotatebel
+{
+	private string[] _up;
+	public abstract string[] Up{ get; set; }
+	private string[] _right;
+	public abstract string[] Right{ get; set; }
+	private string[] _down;
+	public abstract string[] Down{ get; set; }
+	private string[] _left;
+	public abstract string[] Left{ get; set; }
+	Conectors(string[] conector){}
+	Conectors(string[] hor, string[] vert){}
+	Conectors(string[] hor, string[] up, string[] down){}
+	Conectors(string[] left, string[] right, string[] up, string[] down){}
+
+	public abstract int RotationPosition{ get; }
+
+	public abstract void RotateLeft(int times);
+
+	public abstract void RotateRight(int times);
 }
